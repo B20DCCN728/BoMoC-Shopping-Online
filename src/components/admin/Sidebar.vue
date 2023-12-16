0ef9194d4014c5e7f0b3e267275de55f6806ad04
B20DCCN728 <vietjohnny810@gmail.com>
2023-12-16 16:29:58 +0700

    update afternoon

--- src/components/admin/Sidebar.vue
+++ src/components/admin/Sidebar.vue
@@ -5,6 +5,9 @@ DesktopOutlined,
 UserOutlined,
 TeamOutlined,
 FileOutlined,
+MenuOutlined,
+FileDoneOutlined,
+SettingOutlined,
 } from '@ant-design/icons-vue';
 import { ref, defineProps } from 'vue';
 
@@ -34,23 +37,38 @@ const collapsed = ref(false);
                 <span>Khách hàng</span>
               </span>
             </template>
-            <a-menu-item key="3">Tom</a-menu-item>
-            <a-menu-item key="4">Bill</a-menu-item>
-            <a-menu-item key="5">Alex</a-menu-item>
+            <a-menu-item key="3">Danh sách</a-menu-item>
+            <a-menu-item key="4">Danh sách đen</a-menu-item>
+            <a-menu-item key="5">Cài đặt</a-menu-item>
           </a-sub-menu>
           <a-sub-menu key="sub2">
             <template #title>
               <span>
                 <team-outlined />
-                <span>Sản phẩm</span>
+                <span>Pages</span>
               </span>
             </template>
-            <a-menu-item key="6">Quản lí</a-menu-item>
-            <a-menu-item key="8">Tạo bài đăng</a-menu-item>
+            <a-menu-item key="6">404 - Not Found</a-menu-item>
+            <a-menu-item key="8">Coupons</a-menu-item>
           </a-sub-menu>
-          <a-menu-item key="9">
-            <file-outlined />
-            <span>File</span>
+          <a-sub-menu key="sub3">
+            <template #title>
+              <span>
+                <menu-outlined />
+                <span>Catalog</span>
+              </span>
+            </template>
+            <a-menu-item key="9">Sản phẩm </a-menu-item>
+            <a-menu-item key="10">Categories</a-menu-item>
+            <a-menu-item key="11">Phiếu giảm giá</a-menu-item>
+          </a-sub-menu>
+          <a-menu-item key="12">
+            <file-done-outlined />
+            <span>Orders</span>
+          </a-menu-item>
+          <a-menu-item key="13">
+            <setting-outlined />
+            <span>Setting</span>
           </a-menu-item>
         </a-menu>
     </a-layout-sider>
